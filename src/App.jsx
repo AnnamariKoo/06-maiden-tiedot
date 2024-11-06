ad7fe921bb9a94aff9da6c6ea5e2675157d51914
AnnamariKoo <163762866+AnnamariKoo@users.noreply.github.com>
2024-11-06 10:23:25 +0200

    Axios asennettu ja haettu maiden tiedot

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-// import { useState } from "react";
+import { useState, useEffect } from "react";
+import countriesService from "./services/countries.js";
+import Filter from "./components/Filter/Filter.jsx";
 
 const App = () => {
+  const [countries, setCountries] = useState([]),
+    [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    countriesService.getAll().then((response) => {
+      console.log("response", response);
+      setCountries(response);
+    });
+  }, []);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <div>
-      <h2>Hello there!</h2>
+      <Filter />
     </div>
   );
 };
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,10 @@
+const Filter = (props) => {
+  return (
+    <form className="filterForm">
+      Find countries:
+      <input type="text" />
+    </form>
+  );
+};
+
+export default Filter;
